ba78634f8ba72a8ee8573bc921e324f2a112bf0e
Joshua Bates <joshuabates@gmail.com>
2018-05-03 17:06:00 -0600

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,11 +7,17 @@ elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
   ./bootstrap/linux.sh
 fi
 
+python2 -m pip install --upgrade setuptools
+python2 -m pip install --upgrade pip
+python2 -m pip install --user tmuxp
+
 if [[ ! -d "${ZDOTDIR:-$HOME}/.zprezto" ]]; then
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 fi
 
 env RCRC=$HOME/.dotfiles/rcrc rcup
+
+setopt EXTENDED_GLOB
 
 mkdir -p ~/.vim/autoload
 curl -fLo ~/.vim/autoload/plug.vim \
